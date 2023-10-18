280107712c3f28c9b6b7bbda9f70a264705f0db6
DournauxNathan <dournauxnathan@gmail.com>
2023-10-18 11:41:21 +0200

    Request Editor

--- Assets/Scripts/RequestHandler.cs
+++ Assets/Scripts/RequestHandler.cs
@@ -1,24 +1,51 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEditor;
 using UnityEngine.Networking;
 
+#if UNITY_EDITOR
+[CustomEditor(typeof(RequestHandler))]
+public class HttpRequestManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        RequestHandler manager = (RequestHandler)target;
+
+        GUILayout.Space(10);
+                
+        if (GUILayout.Button("GET"))
+        {
+            manager.CallCoroutine("GetRequest");
+        }
+        GUILayout.Space(5);
+        if (GUILayout.Button("POST"))
+        {
+            manager.CallCoroutine("PostRequest");
+        }
+        GUILayout.Space(5);
+        if (GUILayout.Button("PUT"))
+        {
+            manager.CallCoroutine("PutRequest");
+        }
+        GUILayout.Space(5);
+        if (GUILayout.Button("DELETE"))
+        {
+            manager.CallCoroutine("DeleteRequest");
+        }
+    }
+}
+
+[ExecuteInEditMode]
+#endif
 public class RequestHandler : MonoBehaviour
 {
     [SerializeField] private string url = "";
-    [SerializeField] private string jsonData = "";
-    [SerializeField] private string urlToUpdate = "";
-    [SerializeField] private string urlToDelete = "";
+    /*[SerializeField]*/ private Post jsonData;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        StartCoroutine(MakeRequest());
-        /*StartCoroutine(PostRequest());
-        StartCoroutine(PutRequest());
-        StartCoroutine(DeleteRequest());*/
-    }
-
-    IEnumerator MakeRequest()
+    IEnumerator GetRequest()
     {
         using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
         {
@@ -32,30 +59,39 @@ public class RequestHandler : MonoBehaviour
             {
                 // Successfully received a response.
                 string response = webRequest.downloadHandler.text;
-                Debug.Log("Response: " + response);
 
                 JSONPlaceHolder placeHolder = JsonUtility.FromJson<JSONPlaceHolder>(response);
                 Debug.Log(placeHolder.userID);
                 Debug.Log(placeHolder.id);
                 Debug.Log(placeHolder.title);
                 Debug.Log(placeHolder.body);
-
-                /*Pokemon pokemon = JsonUtility.FromJson<Pokemon>(response);
-                Debug.LogError(pokemon.name);*/
             }
         }
     }
 
+    #region FOR NOW - All the method below CANNOT perform their purposes.
+    /* The JSONPlaceholder API provides a read-only, 
+     * mock RESTful API, which means you can retrieve data but 
+     * cannot actually perform POST, PUT, or DELETE operations.
+     * It's designed for practicing and testing HTTP requests 
+     * without making any changes to the server.
+     * 
+     * To practice and develop functionality that involves making 
+     * HTTP requests with real POST, PUT, and DELETE operations, 
+     * you should use a real API or server that allows such modifications.
+     */
     IEnumerator PostRequest()
     {
+        string _jsonData = JsonUtility.ToJson(jsonData);
+
         UnityWebRequest webRequest = new UnityWebRequest(url, "POST");
-        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(jsonData);
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(_jsonData);
         webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
         webRequest.downloadHandler = new DownloadHandlerBuffer();
         webRequest.SetRequestHeader("Content-Type", "application/json");
 
         yield return webRequest.SendWebRequest();
-
+                
         if (webRequest.result != UnityWebRequest.Result.Success)
         {
             Debug.LogError("Error: " + webRequest.error);
@@ -70,7 +106,7 @@ public class RequestHandler : MonoBehaviour
     IEnumerator PutRequest()
     {
         UnityWebRequest webRequest = new UnityWebRequest(url, "PUT");
-        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(urlToUpdate);
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(url);
         webRequest.uploadHandler = new UploadHandlerRaw(bodyRaw);
         webRequest.downloadHandler = new DownloadHandlerBuffer();
         webRequest.SetRequestHeader("Content-Type", "application/json");
@@ -90,7 +126,7 @@ public class RequestHandler : MonoBehaviour
 
     IEnumerator DeleteRequest()
     {
-        UnityWebRequest webRequest = new UnityWebRequest(urlToDelete, "DELETE");
+        UnityWebRequest webRequest = new UnityWebRequest(url, "DELETE");
         webRequest.downloadHandler = new DownloadHandlerBuffer();
 
         yield return webRequest.SendWebRequest();
@@ -105,16 +141,13 @@ public class RequestHandler : MonoBehaviour
             Debug.Log("Response: " + response);
         }
     }
-}
 
-[System.Serializable]
-public class Pokemon 
-{
-    public int id;
-    public Sprite sprites;
-    public string name;
-    public float heigt;
-    public float weight;
+    #endregion
+    
+    public void CallCoroutine(string method)
+    {
+        StartCoroutine(method);
+    }
 }
 
 [System.Serializable]
@@ -126,3 +159,19 @@ public class JSONPlaceHolder
     public string body;
 }
 
+[System.Serializable]
+public class Post
+{
+    public string title;
+    public string body;
+}
+
+[System.Serializable]
+public class Comment
+{
+    public int postId;
+    public int id;
+    public string name;
+    public string email;
+    public string body;
+}
